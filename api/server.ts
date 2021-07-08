62bfa0266ccf9c1067b5be3999de813f11e0b2c9
ioanniskalvis <ioanniskalvis@hotmail.com>
2021-07-08 01:12:05 +0100

    Added a boilerplate for a noedejs and mysql+sequelize api

---
+++ api/server.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express() // initialize the express server
+// create a test route
+app.get('/', (req, res, next) => {
+	res.send('Hello world')
+	console.log('Hello world')
+})
+// Define the port to run the server. this could either be defined // in the environment variables or directly as shown below
+app.listen(process.env.PORT || 4001, () => {
+	console.log('server started')
+})
